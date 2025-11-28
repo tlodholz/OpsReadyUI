57cc2a09e5b0365747fc13355fdf8734cbad1f13
Theodore Lodholz <tlodholz@gmail.com>
2025-11-28 18:04:30 -0500

    Created training models and add entry form

---
+++ OpsReadyUI/OpsReadyUI/Models/TrainingAssignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OpsReadyUI.Models
+{
+    public class TrainingAssignment
+    {
+        public int Id { get; set; }
+        public int TrainingEventId { get; set; }
+        public int UserId { get; set; }
+        public int TrainingRecordId { get; set; }
+        public DateTime AssignedDate { get; set; }
+        public int AssignedByUserId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int? ModifiedByUserId { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
